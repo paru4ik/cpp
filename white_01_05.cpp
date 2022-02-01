0b2a2e8eb91f83860525eb80a76ad30a730e6b09
p0ru4ik <paru4ik@yandex.ru>
2022-02-01 13:29:03 +0300

    discont

---
+++ white_01_05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double n, a, b, x, y;
+    cin >> n >> a >> b >> x >> y;
+    if (n > b) {
+        cout << n - n * y / 100.;
+    } else if (n > a) {
+        cout << n - n * x / 100.;
+    } else {
+        cout << n;
+    }
+    return 0;
+}
